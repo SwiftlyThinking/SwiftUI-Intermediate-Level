fe7eb33c61aafee326557d71105c9ddaf061a689
Mykyta Kuzminov <mykyta.kuzminov26@icloud.com>
2024-02-26 17:53:57 +0200

    Sut up a project

---
+++ SwiftUI-Intermediate-Level/SwiftUI-Intermediate-Level/SwiftUI_Intermediate_LevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Intermediate_LevelApp.swift
+//  SwiftUI-Intermediate-Level
+//
+//  Created by Mykyta Kuzminov on 26.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Intermediate_LevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
